8c62206181a9b2dc90f59c8ea3f5f31794241ede
Valeria Vitvytska <leranmu@gmail.com>
2022-05-16 19:21:00 +0300

    Update 12.js

--- 12.js
+++ 12.js
@@ -5,6 +5,24 @@ window.addEventListener('keydown', (e) => {
 
 	buttons.forEach(button => {
 
+		if (+button.dataset.code === e.keyCode) {
+			button.classList.add('blue')
+		}
+		else {
+			button.classList.remove('blue')
+		}
+	})
+})
+
+
+/* Другой вариант с использованием event.key
+
+Этот вариант решения прекрасно работает,если язык ввода английский ,но если установлен другой язык ввода при нажатии на клавишу, он не работает
+
+window.addEventListener('keydown', (e) => {
+
+	buttons.forEach(button => {
+
 		if (button.innerText === e.key || button.innerText.toLowerCase() === e.key) {
 			button.classList.add('blue')
 		} else {
@@ -12,5 +30,5 @@ window.addEventListener('keydown', (e) => {
 		}
 	})
 })
-
+*/
 
